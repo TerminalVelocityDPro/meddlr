2e15883dd1f28cd844a2a4655a4aa0c2bc1cd7de
Deepro Pasha <deepro.pasha@gmail.com>
2023-09-12 17:58:29 -0700

    spiral working edits

--- run_unet_multi_vortex.sh
+++ run_unet_multi_vortex.sh
@@ -9,17 +9,17 @@ echo "The first available gpu is $gpu"
 
 # Run normal unet yaml
 # took off --debug to allow wb to work. 
-WANDB__SERVICE_WAIT=300 CUDA_VISIBLE_DEVICES=$gpu$ python tools/train_net.py --config-file configs/mri-recon/mridata-3dfse-knee/unrolled/supervised/Supervised_Multi_Aug_UNET_1_Scan.yaml --auto-version
+WANDB__SERVICE_WAIT=300 CUDA_VISIBLE_DEVICES=$gpu$ python tools/train_net.py --config-file configs/mri-recon/mridata-3dfse-knee/unet/supervised/Supervised_Multi_Aug_UNET_1_Scan.yaml --auto-version
 
 gpu=$(python get_available_gpu.py)
 echo "The first available gpu for VORTEX is $gpu"
 
 # Run vortex unet yaml
 # took off --debug to allow wb to work. 
-WANDB__SERVICE_WAIT=300 CUDA_VISIBLE_DEVICES=$gpu$ python tools/train_net.py --config-file configs/mri-recon/mridata-3dfse-knee/unrolled/supervised/Supervised_Multi_Aug_UNET_14_Scan.yaml --auto-version
+WANDB__SERVICE_WAIT=300 CUDA_VISIBLE_DEVICES=$gpu$ python tools/train_net.py --config-file configs/mri-recon/mridata-3dfse-knee/unet/supervised/Supervised_Multi_Aug_UNET_14_Scan.yaml --auto-version
 
 gpu=$(python get_available_gpu.py)
 echo "The first available gpu for VORTEX is $gpu"
 
 # Run vortex unrolled yaml
-WANDB__SERVICE_WAIT=300 CUDA_VISIBLE_DEVICES=$gpu$ python tools/train_net.py --config-file configs/mri-recon/mridata-3dfse-knee/unrolled/ssdu/SSDU_Multi_Aug_UNET_13_Scan.yaml --auto-version
+WANDB__SERVICE_WAIT=300 CUDA_VISIBLE_DEVICES=$gpu$ python tools/train_net.py --config-file configs/mri-recon/mridata-3dfse-knee/unet/ssdu/SSDU_Multi_Aug_UNET_13_Scan.yaml --auto-version
